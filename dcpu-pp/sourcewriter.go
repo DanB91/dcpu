f3e796ab561dcf20c9d9d9da195735e165e7923b
jim teeuwen <jimteeuwen@gmail.com>
2012-04-28 14:53:20 +0200

    Start source writer.

---
+++ dcpu-pp/sourcewriter.go
@@ -0,0 +1,15 @@
+// This file is subject to a 1-clause BSD license.
+// Its contents can be found in the enclosed LICENSE file.
+
+package main
+
+import (
+	"io"
+	"fmt"
+)
+
+// writeSource writes the given AST out as assembly source to the
+// supplied writer.
+func writeSource(w io.Writer, ast *AST) (err error) {
+	return
+}
