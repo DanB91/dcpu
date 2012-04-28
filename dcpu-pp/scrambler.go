cce630804f6fcc0277e3214111d6bdf475a7c181
jim teeuwen <jimteeuwen@gmail.com>
2012-04-28 18:36:01 +0200

    Finish scrambler.

--- dcpu-pp/scrambler.go
+++ dcpu-pp/scrambler.go
@@ -3,6 +3,8 @@
 
 package main
 
+import "fmt"
+
 func init() {
 	Register("scramble", "Obfuscate label names and label references.", NewScrambler)
 }
@@ -15,9 +17,21 @@ func NewScrambler() Processor { return new(Scrambler) }
 func (p *Scrambler) Process(ast *AST) (err error) {
 	var labels []*Label
 	var refs []*Name
+	var i, j int
 
 	findLabels(ast.Root.Children, &labels)
 	findReferences(ast.Root.Children, &refs)
+
+	for i = range labels {
+		old := labels[i].Data
+		labels[i].Data = fmt.Sprintf("l%04x", i)
+
+		for j = range refs {
+			if refs[j].Data == old {
+				refs[j].Data = labels[i].Data
+			}
+		}
+	}
 
 	return
 }
