4bcd7958c0015ae12f59d54a17ab3079bfa59f4d
jim teeuwen <jimteeuwen@gmail.com>
2012-05-03 12:23:47 +0200

    Add sourcereader related files.

---
+++ dcpu-unit/common.go
@@ -0,0 +1,99 @@
+// This file is subject to a 1-clause BSD license.
+// Its contents can be found in the enclosed LICENSE file.
+
+package main
+
+import dp "github.com/jteeuwen/dcpu/parser"
+
+// Instructions and registers according to spec v1.7
+//
+// With an additional 'test' instruction for this tool.
+
+var (
+	instructions = []string{
+		"set", "add", "sub", "mul", "mli", "div", "dvi", "mod", "mdi", "and",
+		"bor", "xor", "shr", "asr", "shl", "ifb", "ifc", "ife", "ifn", "ifg",
+		"ifa", "ifl", "ifu", "adx", "sbx", "sti", "std", "jsr", "int", "iag",
+		"ias", "rfi", "iaq", "hwn", "hwq", "hwi", "dat", "test",
+	}
+
+	regs = []string{
+		"a", "b", "c", "x", "y", "z", "i", "j", "ia",
+		"ex", "peek", "push", "pop", "pc", "sp",
+	}
+)
+
+// isRegister returns true if the given value qualifies as
+// a valid register name.
+func isRegister(v string) bool {
+	for i := range regs {
+		if regs[i] == v {
+			return true
+		}
+	}
+	return false
+}
+
+// isInstruction returns true if the given value qualifies as
+// a valid instruction name.
+func isInstruction(v string) bool {
+	for i := range instructions {
+		if instructions[i] == v {
+			return true
+		}
+	}
+	return false
+}
+
+// findLabels recursively finds Label nodes.
+func findLabels(n []dp.Node, l *[]*dp.Label) {
+	for i := range n {
+		switch tt := n[i].(type) {
+		case dp.NodeCollection:
+			findLabels(tt.Children(), l)
+
+		case *dp.Label:
+			*l = append(*l, tt)
+		}
+	}
+}
+
+// findReferences recursively finds Label references.
+func findReferences(n []dp.Node, l *[]*dp.Name) {
+	for i := range n {
+		switch tt := n[i].(type) {
+		case dp.NodeCollection:
+			findReferences(tt.Children(), l)
+
+		case *dp.Name:
+			if isRegister(tt.Data) || isInstruction(tt.Data) {
+				continue
+			}
+
+			*l = append(*l, tt)
+		}
+	}
+}
+
+// stripDuplicateNames removes duplicate entries from the given Name list.
+func stripDuplicateNames(r []*dp.Name) []*dp.Name {
+	l := make([]*dp.Name, 0, len(r))
+
+	for i := range r {
+		if !containsName(l, r[i].Data) {
+			l = append(l, r[i])
+		}
+	}
+
+	return l
+}
+
+// containsName returns true if the given list contains the supplied Name.
+func containsName(r []*dp.Name, data string) bool {
+	for i := range r {
+		if r[i].Data == data {
+			return true
+		}
+	}
+	return false
+}
---
+++ dcpu-unit/sourcereader.go
@@ -0,0 +1,165 @@
+// This file is subject to a 1-clause BSD license.
+// Its contents can be found in the enclosed LICENSE file.
+
+package main
+
+import (
+	dp "github.com/jteeuwen/dcpu/parser"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// readSource reads the given file and parses its contents
+// into the given AST.
+func readSource(ast *dp.AST, file string) error {
+	fd, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	defer fd.Close()
+	return ast.Parse(fd, file)
+}
+
+// resolveIncludes finds references to undefined labels.
+// It then tries to find the code for these labels in the supplied
+// include paths. Files should be defined as '<labelname>.dasm'.
+func resolveIncludes(ast *dp.AST, includes []string) (err error) {
+	var labels []*dp.Label
+	var refs []*dp.Name
+
+	findLabels(ast.Root.Children(), &labels)
+	findReferences(ast.Root.Children(), &refs)
+
+	refs = findUndefinedRefs(refs, labels)
+	refs = stripDuplicateNames(refs)
+
+	if len(refs) == 0 {
+		// No undefined references. We're done here.
+		return
+	}
+
+	if len(includes) == 0 {
+		// We have unresolved references, but no places to look
+		// for their implementation. This constitutes a booboo.
+		return dp.NewParseError(ast.Files[refs[0].File()], refs[0].Line(), refs[0].Col(),
+			"Undefined reference: %q", refs[0].Data)
+	}
+
+	for i := range refs {
+		if err = loadInclude(ast, includes, refs[i]); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// loadInclude tries to load the given reference as an include file.
+// Parses it into the supplied AST and verifies that it contains what
+// we are looking for.
+func loadInclude(ast *dp.AST, includes []string, r *dp.Name) (err error) {
+	var file string
+
+	name := r.Data + ".dasm"
+	walker := func(f string, info os.FileInfo, err error) error {
+		if filepath.Base(f) == name {
+			file = f
+			return io.EOF // Signal walker to stop.
+		}
+
+		return nil
+	}
+
+	for i := range includes {
+		filepath.Walk(includes[i], walker)
+
+		if len(file) > 0 {
+			break
+		}
+	}
+
+	if len(file) == 0 {
+		return dp.NewParseError(ast.Files[r.File()], r.Line(), r.Col(),
+			"Undefined reference: %q", r.Data)
+	}
+
+	if err = readSource(ast, file); err != nil {
+		return
+	}
+
+	// Test if the code actually contains the label we are looking for.
+	if !includeHasLabel(ast, file, r.Data) {
+		return dp.NewParseError(ast.Files[r.File()], r.Line(), r.Col(),
+			"Undefined reference: %q. Include file was found, but "+
+				"it did not define the desired label.", r.Data)
+	}
+
+	// This new file may hold its own include requirements.
+	return resolveIncludes(ast, includes)
+}
+
+// findUndefinedRefs compares both given lists of labels and
+// label references. Any reference that is not present in the
+// label list, is considered unresolved and added to the 
+// returned list.
+func findUndefinedRefs(refs []*dp.Name, labels []*dp.Label) []*dp.Name {
+	var i, j int
+
+outer:
+	for i = range refs {
+		for j = range labels {
+			if labels[j].Data == refs[i].Data {
+				copy(refs[i:], refs[i+1:])
+				refs = refs[:len(refs)-1]
+				goto outer
+			}
+		}
+	}
+
+	return refs
+}
+
+// includeHasLabel checks if a newly parsed include actually
+// contains the label reference we are looking for.
+func includeHasLabel(ast *dp.AST, file string, target string) bool {
+	index := fileIndex(ast, file)
+	if index == -1 {
+		return false
+	}
+
+	return hasLabel(ast.Root.Children(), index, target)
+}
+
+// hasLabel recursively finds a specific label definition.
+// Returns true if it was found. False otherwise.
+func hasLabel(n []dp.Node, file int, target string) bool {
+	for i := range n {
+		switch tt := n[i].(type) {
+		case dp.NodeCollection:
+			if hasLabel(tt.Children(), file, target) {
+				return true
+			}
+
+		case *dp.Label:
+			if tt.File() == file && tt.Data == target {
+				return true
+			}
+
+			// @_@ These are not the labels you are looking for. @_@
+		}
+	}
+
+	return false
+}
+
+// fileIndex returns the given file's index as it is stored in the AST.
+func fileIndex(ast *dp.AST, file string) int {
+	for i := range ast.Files {
+		if ast.Files[i] == file {
+			return i
+		}
+	}
+	return -1
+}
