4b3b433a434f1badbd7493d493da06d761a53c4e
jim teeuwen <jimteeuwen@gmail.com>
2012-05-04 19:02:47 +0200

    LEM1802: Define default Palette.

--- cpu/hw/lem1802/palette.go
+++ cpu/hw/lem1802/palette.go
@@ -7,6 +7,20 @@ import "github.com/jteeuwen/dcpu/cpu"
 
 // DefaultPalette defines the default colour palette for the LEM1802 monitor.
 var DefaultPalette = []cpu.Word{
-	0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,
+	0x0000, // black
+	0x0007, // dark blue
+	0x0070, // dark green
+	0x0077, // dark teal
+	0x0700, // dark red
+	0x0707, // dark purple
+	0x0770, // dark yellow
+	0x0555, // dark gray
+	0x0aaa, // light gray
+	0x000f, // blue
+	0x00f0, // green
+	0x00ff, // teal
+	0x0f00, // red
+	0x0f0f, // purple
+	0x0ffF, // yellow
+	0x0fff, // white
 }
