cc1678ca0b7d34d1c7b5fdf3a45d27ea907882fc
jimt <jimteeuwen@gmail.com>
2012-07-13 21:18:33 +0200

    Allow dcpu-prof to be run without interactive mode. By specifying the desired command through commandline switches. it just prints the result and exits.

--- dcpu-prof/command.go
+++ dcpu-prof/command.go
@@ -44,33 +44,3 @@ func Handle(prof *prof.Profile, str []string) {
 		list(prof, *filemode, reg)
 	}
 }
-
-func usage() {
-	fmt.Println(`List of known commands:
-
- top [options]
-   List the top N number of samples for all function calls where.
-   
-       -n : The number of results to limit the output to.
-       -s : The sort value denotes the field by which the table should be
-            sorted. Possible values are:
-
-            count: This sorts by number of times each entry has been called.
-             cost: This sorts by the total cycle cost over the entire program's
-                   runtime. This is the default sorting mode.
-    -file : Display usage stats per file instead of functions.
-
- list [options]
-   This gives an instruction-by-instruction listing of cpu cycle usage for
-   all entries that match the given filter.
-
-       -f : This is expected to be a regular expression pattern which will be
-            matched against labels or file names. It defaults to 'match everything'.
-            Note that for a large codebase, this can generate a large amount of
-            output.
-
-            For best results, use the list command in conjunction with 'top' to
-            tell you what code needs closer examination.
-    -file : Display usage stats per file instead of functions.
-`)
-}
--- dcpu-prof/main.go
+++ dcpu-prof/main.go
@@ -12,8 +12,40 @@ import (
 	"path/filepath"
 )
 
+var (
+	topcmd   = flag.Bool("top", false, "")
+	listcmd  = flag.Bool("list", false, "")
+	count    = flag.Uint("n", DefaultTopCount, "")
+	sort     = flag.String("s", DefaultTopSort, "")
+	filter   = flag.String("f", DefaultListFilter, "")
+	filemode = flag.Bool("file", false, "")
+)
+
 func main() {
 	prof := parseArgs()
+
+	// Run program once for a specific command,
+	// or go into interactive mode?
+	if *topcmd || *listcmd {
+		if *topcmd {
+			Handle(prof, []string{
+				"top",
+				"-s", *sort,
+				"-n", fmt.Sprintf("%d", *count),
+				fmt.Sprintf("-file=%v", *filemode),
+			})
+		} else {
+			Handle(prof, []string{
+				"list",
+				"-f", *filter,
+				fmt.Sprintf("-file=%v", *filemode),
+			})
+		}
+
+		return
+	}
+
+	// Interactive mode.
 	input := pollInput()
 
 	for {
@@ -30,8 +62,13 @@ func parseArgs() *prof.Profile {
 		flag.PrintDefaults()
 	}
 
-	help := flag.Bool("h", false, "Display this help.")
-	version := flag.Bool("v", false, "Display version information.")
+	help := flag.Bool("h", false, "")
+	version := flag.Bool("v", false, "")
+
+	flag.Usage = func() {
+		fmt.Printf("Usage %s [options] <file>\n\n", os.Args[0])
+		usage()
+	}
 
 	flag.Parse()
 
@@ -66,4 +103,34 @@ func parseArgs() *prof.Profile {
 	}
 
 	return p
+}
+
+func usage() {
+	fmt.Println(`List of known commands:
+
+ -top [-n -s -file]
+   List the top N number of samples for all function calls where.
+   
+       -n : The number of results to limit the output to.
+       -s : The sort value denotes the field by which the table should be
+            sorted. Possible values are:
+
+            count: This sorts by number of times each entry has been called.
+             cost: This sorts by the total cycle cost over the entire program's
+                   runtime. This is the default sorting mode.
+    -file : Display usage stats per file instead of functions.
+
+ -list [-f -file]
+   This gives an instruction-by-instruction listing of cpu cycle usage for
+   all entries that match the given filter.
+
+       -f : This is expected to be a regular expression pattern which will be
+            matched against labels or file names. It defaults to
+            'match everything'. Note that for a large codebase, this can
+            generate a large amount of output.
+
+            For best results, use the list command in conjunction with 'top' to
+            tell you what code needs closer examination.
+    -file : Display usage stats per file instead of functions.
+`)
 }
