6cc9149ed78f69dc39336f2c7f75e08808c78ee5
jimt <jimteeuwen@gmail.com>
2012-07-20 13:36:39 +0200

    dcpu-ide: * Remove verbose outputs from compress_data.sh

--- dcpu-ide/compress_data.sh
+++ dcpu-ide/compress_data.sh
@@ -23,21 +23,18 @@ htmlcompressor "$SRC" -o "$DST/" -r --remove-intertag-spaces;
 # Minify Javascript files.
 echo "[*] Minify Javascript files...";
 for FILE in `find "$DST" -type f -name "*.js"`; do
-	echo "    $FILE";
 	yuicompressor -o "$FILE" "$FILE";
 done
 
 # Minify stylesheets.
 echo "[*] Minify CSS files...";
 for FILE in `find "$DST" -type f -name "*.css"`; do
-	echo "    $FILE";
 	yuicompressor -o "$FILE" "$FILE";
 done
 
 # Crush PNG files.
 echo "[*] Crushing PNG files...";
 for FILE in `find "$DST" -type f -name "*.png"`; do
-	echo "    $FILE";
 	pngcrush -q -z 3 -reduce "$FILE" "$FILE.new" 1>/dev/null;
 
 	if [ $? -ne 0 ]; then
