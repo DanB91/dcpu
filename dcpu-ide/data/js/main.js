a8f8bc072385ba83163d79d53f95b5191b279148
jimt <jimteeuwen@gmail.com>
2012-07-21 19:22:24 +0200

    dcpu-ide: Add (un)lockApplication functions.
    These are called by dialogs when they are opened/closed and
    serve to lock certain elements out from keyboard input while
    we are in modal mode.
    
    They also maintain a stack of open dialogs, since we can
    have arbitrarily miny of them open at once. Only the top-most
    dialog will ever receive input. The explicit locking of other
    components ensures that the TAB key does not shift focus
    into places it shouldn't be.

--- dcpu-ide/data/js/main.js
+++ dcpu-ide/data/js/main.js
@@ -3,6 +3,7 @@
 
 const AppTitle = 'dcpu-ide &#x2af9;&#x2afa;';
 
+var dialogs      = [];
 var stateTracker = null;
 var workspace    = null;
 var dashboard    = null;
@@ -56,6 +57,87 @@ window.onload = function ()
 	document.onmouseup = onMouseUp;
 	document.onmousewheel = onMouseWheel;
 };
+
+// lockApplication locks various nodes to prevent tab input to them,
+// This is called by the given modal dialog when it is opened.
+function lockApplication(dlg)
+{
+	// Update a list of open dialogs.
+	dialogs.push(dlg);
+
+	if (dialogs.length > 1) {
+		return; // Already locked.
+	}
+
+	var nodelists = [].concat(
+		findLockableNodes(dashboard.node),
+		findLockableNodes(workspace.node)
+	)
+	
+	for (var i = 0; i < nodelists.length; i++) {
+		for (var j = 0; j < nodelists[i].length; j++) {
+			lock(nodelists[i][j]);
+		}
+	}
+}
+
+// unlockApplication unlocks various nodes to re-enable tab input to them,
+// This is called by the given modal dialog when it is closed.
+function unlockApplication(dlg)
+{
+	// Update a list of open dialogs.
+	dialogs.pop();
+
+	if (dialogs.length > 0) {
+		return; // Not as long as their are dialogs.
+	}
+
+	var nodelists = [].concat(
+		findLockableNodes(dashboard.node),
+		findLockableNodes(workspace.node)
+	)
+	
+	for (var i = 0; i < nodelists.length; i++) {
+		for (var j = 0; j < nodelists[i].length; j++) {
+			unlock(nodelists[i][j]);
+		}
+	}
+}
+
+// findLockableNodes finds all children of the given node
+// which we consider relevant for locking/unlocking.
+function findLockableNodes(e)
+{
+	return [].concat(
+		e.getElementsByTagName('a'),
+		e.getElementsByTagName('select'),
+		e.getElementsByTagName('input'),
+		e.getElementsByTagName('button'),
+		e.getElementsByTagName('textarea')
+	);
+}
+
+// lock locks the given control.
+// It is disabled to prevent it from getting key input.
+function lock (e)
+{
+	if (e == undefined || e.attributes['disabled'] != undefined) {
+		return; // We don't want to unlock it later when it shouldn't be.
+	}
+
+	e.setAttribute('disabled', 'disabled');
+	e._locked = true;
+}
+
+// unlock unlocks the given control.
+// It is (re-)enabled to allow key input.
+function unlock (e)
+{
+	if (e != undefined && e._locked == true) {
+		e.removeAttribute('disabled');
+		e._locked = false;
+	}
+}
 
 function onKeyDown (e)
 {
